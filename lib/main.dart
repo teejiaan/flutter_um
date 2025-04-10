71a2d8717d971472f08af8788fe86288e1820320
teejiaan <darenteejiaan@gmail.com>
2025-04-10 14:07:53 +0800

    Changed Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
+      title: 'UM Hackathon 2025 App',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: appRoutes,
