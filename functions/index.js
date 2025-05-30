ff51d7aab1015ca30a2ca49dee322a3fad0f1598
teejiaan <darenteejiaan@gmail.com>
2025-05-31 01:25:08 +0800

    took out secret

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const stripe = require('stripe')('sk_test_51RU7OjPneyNRrgC5B7oSGjSnJ6i7hjeD8KkoIbKW5RnPRHTcxPyKLUvMYOACjfcpKjutMw9aT3EuOFLbBmbKf4G300We1H2Sn8');
+const stripe = require('stripe')('//secret_key_here');
 
 admin.initializeApp();
 
